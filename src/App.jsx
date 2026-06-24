f5dcdbcc285612659fced25b17b8a481da2a118b
Anishojha006 <anishojha1512@gmail.com>
2026-06-24 19:27:00 +0530

    working on footer navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Section from './components/Section'
 import Ourservices from './components/Ourservices'
 import Section2 from './components/Section2'
 import OurTeam from './components/OurTeam'
+import Section3 from './components/Section3'
 
 const App = () => {
   return (
@@ -13,6 +14,8 @@ const App = () => {
       <Ourservices/>
       <Section2/>
       <OurTeam/>
+      <Section3/>
+      <Navbar/>
     </div>
   )
 }
---
+++ src/components/Section3.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import calendersvg from '../assets/calendericon.svg';
+
+const Section3 = () => {
+  return (
+    <div className='Section3'>
+      <h1>Have an idea ?</h1>
+      <h1>Let's talk.</h1>
+      <p>Visually attractive design from concept to final result. We create solutions that are bold and forward-looking.</p>
+      <button>  <img src={calendersvg}/>Free Consultataion </button>
+    </div>
+  )
+}
+
+export default Section3
