e715c0c14ce205596027e5b391414d8cc53e0dfe
Anishojha006 <anishojha1512@gmail.com>
2026-06-27 19:42:35 +0530

    completed the ui

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,25 @@ import Section2 from './components/Section2'
 import OurTeam from './components/OurTeam'
 import Section3 from './components/Section3'
 
+
 const App = () => {
   return (
-    <div className='PaentNav'>
-      <Navbar/>
-      <Section/>
-      <Ourservices/>
-      <Section2/>
-      <OurTeam/>
-      <Section3/>
-      <Navbar/>
+    <div className='PaentNav' >
+      <Navbar img={"src/assets/Hamburger.svg"} calender={"src/assets/GetinTouch.svg"} calenderText="Get in Touch" union="src/assets/Union.svg" unionText="Services" />
+      <Section />
+      <Ourservices />
+      <Section2 />
+      <OurTeam />
+      <Section3 />
+     <Navbar
+  text="Compser"
+  email="hello@Compser.io"
+  calenderText="© 2088 Nayzak Design"
+  img1={"src/assets/img1.png"}
+  img2={"src/assets/img2.png"}
+  img3={"src/assets/img3.png"}
+  img4={"src/assets/img4.png"}
+/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
 import img from '../assets/Branding.svg'
-import hamburger from '../assets/hamburger.svg'
 import union from '../assets/Union.svg'
 import getintouch from '../assets/GetinTouch.svg'
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
-   <div className='navbar'>
-    <div className="div1">
-      <img src={img} alt="" />
+    <div className='navbar'>
+      <div className="div1">
+        <img src={img} alt="" />
+        <span>{props.text}</span>
+      </div>
+      <div className="div2">
+        <h5>Project</h5>
+        <h5>About</h5>
+        <h5>Team</h5>
+        <h5>Contact</h5>
+      </div>
+      <div className="div3">
+        <img src={props.img} />
+        <h2>{props.email}</h2>
+      </div>
+      <div className="div4">
+        <p> <img src={props.union} />  {props.unionText} </p>
+      <div className='contact-Pages'>
+          <img src={props.img1} />
+        <img src={props.img2} />
+        <img src={props.img3} />
+         <img src={props.img4} />
+      </div>
+      </div>
+      <div className="div5">
+        <p><img src={props.calender} /> {props.calenderText}</p>
+
+      </div>
     </div>
-    <div className="div2">
-      <h5>Project</h5>
-      <h5>About</h5>
-      <h5>Team</h5>
-      <h5>Contact</h5>
-    </div>
-    <div className="div3">
-      <img src={hamburger} alt="" />
-    </div>
-    <div className="div4">
-      <p> <img src={union} />  Services </p>
-    </div>
-    <div className="div5">
-      <p><img src={getintouch}/> Get in touch </p>
-    </div>
-   </div>
   )
 }
 
